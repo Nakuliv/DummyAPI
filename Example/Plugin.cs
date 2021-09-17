950361e715decf686a7007051a440242bba47495
Cwaniaak <ccwaniaktv@gmail.com>
2021-09-17 18:55:32 +0200

    added example

---
+++ Example/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using Exiled.API.Features;
+using System;
+using Handlers = Exiled.Events.Handlers;
+
+namespace Example
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author { get; } = "Naku";
+        public override string Name { get; } = "Example";
+        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
+
+        public override void OnEnabled()
+        {
+            Plugin.Singleton = this;
+
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+
+            base.OnDisabled();
+        }
+
+
+        public static Plugin Singleton;
+    }
+}
---
+++ Example/SpawnDummy.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandSystem;
+using DummyAPI;
+using Exiled.API.Features;
+using Exiled.Permissions.Extensions;
+using MEC;
+using UnityEngine;
+
+namespace Example
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    internal class SpawnDummy : ICommand
+    {
+        private Dummy _dummy;
+        public string Command { get; } = "spawnscp372";
+
+        public string[] Aliases { get; } = new string[]
+        {
+            "spawndummy",
+            "spdummy",
+        };
+
+        public string Description { get; } = "Spawn SCP-372";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Player player = Player.Get(arguments.At(0));
+            if (!Permissions.CheckPermission((CommandSender)sender, "dummy.spawn"))
+            {
+                response = "You do not have permissions to run this command!";
+                return false;
+            }
+
+            if (arguments.Count != 1)
+            {
+                response = "Usage: spawndummy (player id / name)";
+                return false;
+            }
+
+            if (player == null)
+            {
+                response = $"Player not found: {arguments.At(0)}";
+                return false;
+            }
+
+            _dummy = new Dummy(player.Position, new Quaternion(), RoleType.Tutorial);
+            Timing.RunCoroutine(Walk());
+            response = $"Spawned Dummy!";
+            return true;
+        }
+
+        private IEnumerator<float> Walk()
+        {
+            for (; ; )
+            {
+                var _dood = Player.Get(2);
+                yield return Timing.WaitForSeconds(0.1f);
+
+                _dummy.Movement = PlayerMovementState.Walking;
+                _dummy.Direction = MovementDirection.Forward;
+                _dummy.RotateToPosition(_dood.Position);
+                float distance = Vector3.Distance(_dood.Position, _dummy.Position);
+
+                if (distance <= 1.25f)
+                    _dummy.Direction = MovementDirection.Stop;
+            }
+        }
+    }
+}
